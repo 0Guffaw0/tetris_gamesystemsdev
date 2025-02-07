7c181e01a20c3d7d7d75f6093a3b83e48e30ccd6
0Guffaw0 <animarowen@gmail.com>
2025-02-07 16:35:51 -0500

    putting unity file into repo
    da game

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,242 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[DefaultExecutionOrder(-1)]
+public class Board : MonoBehaviour
+{
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
+    public Piece nextPiece { get; private set; }
+    public Piece savedPiece { get; private set; }
+
+    public TetrominoData[] tetrominoes;
+
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+    public Vector3Int spawnPosition = new Vector3Int(-1, 8, 0);
+    public Vector3Int previewPosition = new Vector3Int(-1, 12, 0);
+    public Vector3Int holdPosition = new Vector3Int(-1, 16, 0);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-boardSize.x / 2, -boardSize.y / 2);
+            return new RectInt(position, boardSize);
+        }
+    }
+
+    private void Awake()
+    {
+        tilemap = GetComponentInChildren<Tilemap>();
+        activePiece = GetComponentInChildren<Piece>();
+
+        nextPiece = gameObject.AddComponent<Piece>();
+        nextPiece.enabled = false;
+
+        savedPiece = gameObject.AddComponent<Piece>();
+        savedPiece.enabled = false;
+
+        for (int i = 0; i < tetrominoes.Length; i++)
+        {
+            tetrominoes[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SetNextPiece();
+        SpawnPiece();
+    }
+
+    private void SetNextPiece()
+    {
+        // Clear the existing piece from the board
+        if (nextPiece.cells != null)
+        {
+            Clear(nextPiece);
+        }
+
+        // Pick a random tetromino to use
+        int random = Random.Range(0, tetrominoes.Length);
+        TetrominoData data = tetrominoes[random];
+
+        // Initialize the next piece with the random data
+        // Draw it at the "preview" position on the board
+        nextPiece.Initialize(this, previewPosition, data);
+        Set(nextPiece);
+    }
+
+    public void SpawnPiece()
+    {
+        // Initialize the active piece with the next piece data
+        activePiece.Initialize(this, spawnPosition, nextPiece.data);
+
+        // Only spawn the piece if valid position otherwise game over
+        if (IsValidPosition(activePiece, spawnPosition))
+        {
+            Set(activePiece);
+        }
+        else
+        {
+            GameOver();
+        }
+
+        // Set the next random piece
+        SetNextPiece();
+    }
+
+    public void SwapPiece()
+    {
+        // Temporarily store the current saved data so we can swap
+        TetrominoData savedData = savedPiece.data;
+
+        // Clear the existing saved piece from the board
+        if (savedData.cells != null)
+        {
+            Clear(savedPiece);
+        }
+
+        // Store the next piece as the new saved piece
+        // Draw this piece at the "hold" position on the board
+        savedPiece.Initialize(this, holdPosition, nextPiece.data);
+        Set(savedPiece);
+
+        // Swap the saved piece to be the next piece
+        if (savedData.cells != null)
+        {
+            // Clear the existing next piece before swapping
+            Clear(nextPiece);
+
+            // Re-initialize the next piece with the saved data
+            // Draw this piece at the "preview" position on the board
+            nextPiece.Initialize(this, previewPosition, savedData);
+            Set(nextPiece);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            SwapPiece();
+        }
+    }
+
+    public void GameOver()
+    {
+        tilemap.ClearAllTiles();
+        Debug.Log("Game Over!");
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
+
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = Bounds;
+
+        // The position is only valid if every cell is valid
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            // An out of bounds tile is invalid
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            // A tile already occupies the position, thus invalid
+            if (tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void ClearLines()
+    {
+        RectInt bounds = Bounds;
+        int row = bounds.yMin;
+
+        // Clear from bottom to top
+        while (row < bounds.yMax)
+        {
+            // Only advance to the next row if the current is not cleared
+            // because the tiles above will fall down when a row is cleared
+            if (IsLineFull(row))
+            {
+                LineClear(row);
+            }
+            else
+            {
+                row++;
+            }
+        }
+
+
+    }
+
+    public bool IsLineFull(int row)
+    {
+        RectInt bounds = Bounds;
+
+        for (int col = bounds.xMin; col < bounds.xMax; col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+
+            // The line is not full if a tile is missing
+            if (!tilemap.HasTile(position))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void LineClear(int row)
+    {
+        RectInt bounds = Bounds;
+
+        // Clear all tiles in the row
+        for (int col = bounds.xMin; col < bounds.xMax; col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+            tilemap.SetTile(position, null);
+        }
+
+        // Shift every row above down one
+        while (row < bounds.yMax)
+        {
+            for (int col = bounds.xMin; col < bounds.xMax; col++)
+            {
+                Vector3Int position = new Vector3Int(col, row + 1, 0);
+                TileBase above = tilemap.GetTile(position);
+
+                position = new Vector3Int(col, row, 0);
+                tilemap.SetTile(position, above);
+            }
+
+            row++;
+        }
+    }
+
+}
